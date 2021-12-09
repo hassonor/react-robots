5fefe468b88ee2d3ffa2a08de19cb675b560980f
Or Hasson <hassonor@gmail.com>
2021-12-09 20:34:07 +0200

    cleanup code

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -1,11 +1,9 @@
-import React from 'react';
-
 const Scroll = (props) => {
-  return (
-    <div style={{ overflow: 'scroll', border: '5px solid black', height: '800px'}}>
-      {props.children}
-    </div>
-  );
+    return (
+        <div style={{overflow: 'scroll', border: '5px solid black', height: '800px'}}>
+            {props.children}
+        </div>
+    );
 };
 
 export default Scroll;
