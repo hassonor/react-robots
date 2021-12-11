64e8ba73b4f57cf88bcae608769e9a3396370394
Or Hasson <hassonor@gmail.com>
2021-12-12 00:01:56 +0200

    adding home

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,8 +1,7 @@
 const Card = ({name, email, id}) => {
     return (
         <div className='tc grow bg-light-green br3 pa3 ma2 dib bw2 shadow-5'>
-            <img alt='robots' width="200" height="200"
-                 src={`https://robohash.org/${id}?size=200x200`}/>
+            <img alt='robots' src={`https://robohash.org/${id}?size=200x200`}/>
             <div>
                 <h2>{name}</h2>
                 <p>{email}</p>
