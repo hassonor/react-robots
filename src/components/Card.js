d53d2b82e9babb82641663a7df394648b6d89644
Or Hasson <hassonor@gmail.com>
2021-12-11 22:34:32 +0200

    improve perfromance

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,7 +2,7 @@ const Card = ({name, email, id}) => {
     return (
         <div className='tc grow bg-light-green br3 pa3 ma2 dib bw2 shadow-5'>
             <img alt='robots' style={{weight: '200px', height: '200px'}}
-                 src={`https://robohash.org/${id}`}/>
+                 src={`https://robohash.org/${id}?size=200x200`}/>
             <div>
                 <h2>{name}</h2>
                 <p>{email}</p>
--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -2,6 +2,7 @@ const SearchBox = ({searchChange}) => {
     return (
         <div className='pa2'>
             <input
+                aria-label='Search Robots'
                 className='pa3 ba b--green bg-lightest-blue'
                 type='search'
                 placeholder='Search Robots'
