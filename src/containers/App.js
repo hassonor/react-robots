d6f9ca518d647af463d548fda120d2b641e32930
Or Hasson <hassonor@gmail.com>
2021-12-11 18:10:40 +0200

    Adding React.memo and ShouldComponentUpdate

--- src/containers/App.js
+++ src/containers/App.js
@@ -6,6 +6,7 @@ import Scroll from '../components/Scroll';
 import ErrorBoundary from "../components/ErrorBoundary";
 import './App.css';
 import {requestRobots, setSearchfield} from "../actions";
+import Header from "../components/Header";
 
 
 const mapStateToProps = state => {
@@ -41,7 +42,7 @@ function App(props) {
         <h1>Loading...</h1> :
         (
             <div className='tc'>
-                <h1 className='f1'>React Robots</h1>
+                <Header/>
                 <SearchBox searchChange={onSearchChange}/>
                 <Scroll>
                     <ErrorBoundary>
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import {createStore, applyMiddleware, combineReducers} from "redux";
-import {createLogger} from 'redux-logger';
 import thunkMiddleware from 'redux-thunk';
 import {searchRobots, requestRobots} from "./reducers";
 import App from "./containers/App";
@@ -10,9 +9,9 @@ import reportWebVitals from './reportWebVitals';
 import './index.css';
 import 'tachyons';
 
-const logger = createLogger();
+// const logger = createLogger();
 const rootReducer = combineReducers({searchRobots, requestRobots});
-const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, logger));
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
 
 ReactDOM.render(
     <React.StrictMode>
