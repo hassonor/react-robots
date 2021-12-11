2480233afce5e01fb39cdd9060932537f7f476aa
Or Hasson <hassonor@gmail.com>
2021-12-11 23:12:08 +0200

    revert commits

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ import './index.css';
 import 'tachyons';
 import * as serviceWorker from './serviceWorker'
 
+
 // const logger = createLogger();
 const rootReducer = combineReducers({searchRobots, requestRobots});
 const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
