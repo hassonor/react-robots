0d9982ebd1587b243a77f9abdda2735bdc3545d5
Or Hasson <hassonor@gmail.com>
2021-12-11 22:19:54 +0200

    imporve seo score

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,8 @@
 const Card = ({name, email, id}) => {
     return (
         <div className='tc grow bg-light-green br3 pa3 ma2 dib bw2 shadow-5'>
-            <img alt='robots' src={`https://robohash.org/${id}?size=200x200`}/>
+            <img alt='robots' style={{weight: '200px', height: '200px'}}
+                 src={`https://robohash.org/${id}`}/>
             <div>
                 <h2>{name}</h2>
                 <p>{email}</p>
